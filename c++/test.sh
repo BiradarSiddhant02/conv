b61f809ceb2e6158a99521099e2de1a40ab74ce7
BiradarSiddhant02 <pes1202102033@pesu.pes.edu>
2024-06-15 19:25:04 +0530

    app in c++

--- c++/test.sh
+++ c++/test.sh
@@ -6,50 +6,50 @@ clear
 # Test invalid mode
 echo
 echo "Testing invalid mode:"
-echo "./build/conv -m oct -l 32 -n 1a2b3c"
-./build/conv -m oct -l 32 -n 1a2b3c
+echo "conv -m oct -l 32 -n 1a2b3c"
+conv -m oct -l 32 -n 1a2b3c
 
 # Test invalid length
 echo "Testing invalid length:"
-echo "./build/conv -m hex -l 16 -n 1a2b3c"
-./build/conv -m hex -l 16 -n 1a2b3c
-echo "./build/conv -m hex -l 128 -n 1a2b3c"
-./build/conv -m hex -l 128 -n 1a2b3c
+echo "conv -m hex -l 16 -n 1a2b3c"
+conv -m hex -l 16 -n 1a2b3c
+echo "conv -m hex -l 128 -n 1a2b3c"
+conv -m hex -l 128 -n 1a2b3c
 
 # Test invalid number
 echo
 echo "Testing invalid number:"
-echo "./build/conv -m hex -l 32 -n hello"
-./build/conv -m hex -l 32 -n hello
-echo "./build/conv -m bin -l 32 -n 1020"
-./build/conv -m bin -l 32 -n 1020
+echo "conv -m hex -l 32 -n hello"
+conv -m hex -l 32 -n hello
+echo "conv -m bin -l 32 -n 1020"
+conv -m bin -l 32 -n 1020
 
 # Test missing arguments
 echo
 echo "Testing missing arguments:"
-echo "./build/conv -m hex -l 32"
-./build/conv -m hex -l 32
-echo "./build/conv -m hex -n 1a2b3c"
-./build/conv -m hex -n 1a2b3c
-echo "./build/conv -l 32 -n 1a2b3c"
-./build/conv -l 32 -n 1a2b3c
-./build/conv
+echo "conv -m hex -l 32"
+conv -m hex -l 32
+echo "conv -m hex -n 1a2b3c"
+conv -m hex -n 1a2b3c
+echo "conv -l 32 -n 1a2b3c"
+conv -l 32 -n 1a2b3c
+conv
 
 # Test edge cases
 echo
 echo "Testing edge cases:"
-echo "./build/conv -m hex -l 32 -n ffffffff"
-./build/conv -m hex -l 32 -n ffffffff
-echo "./build/conv -m dec -l 32 -n 4294967295"
-./build/conv -m dec -l 32 -n 4294967295
-echo "./build/conv -m bin -l 32 -n 11111111111111111111111111111111"
-./build/conv -m bin -l 32 -n 11111111111111111111111111111111
+echo "conv -m hex -l 32 -n ffffffff"
+conv -m hex -l 32 -n ffffffff
+echo "conv -m dec -l 32 -n 4294967295"
+conv -m dec -l 32 -n 4294967295
+echo "conv -m bin -l 32 -n 11111111111111111111111111111111"
+conv -m bin -l 32 -n 11111111111111111111111111111111
 
 # Test valid inputs
 echo "Testing valid inputs:"
-echo "./build/conv -m hex -l 32 -n 1ab3c # check"
-./build/conv -m hex -l 32 -n 1ab3c # check
-echo "./build/conv -m dec -l 64 -n 1234567890"
-./build/conv -m dec -l 64 -n 1234567890
-echo "./build/conv -m bin -l 32 -n 000001"
-./build/conv -m bin -l 32 -n 000001
+echo "conv -m hex -l 32 -n 1ab3c # check"
+conv -m hex -l 32 -n 1ab3c # check
+echo "conv -m dec -l 64 -n 1234567890"
+conv -m dec -l 64 -n 1234567890
+echo "conv -m bin -l 32 -n 000001"
+conv -m bin -l 32 -n 000001
